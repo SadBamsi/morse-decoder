bdcff97d58bf0706cd4e4027953121f9cea3b55b
omaslov <omaslov@xbsoftware.by>
2020-10-02 12:21:01 +0300

    dev: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,26 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {    
+        let length = expr.length;
+        let res = []
+        expr = expr.split('');
+        for (let i = 0; i < length; i += 10) {
+            let letter = [];
+            let substring = expr.splice(0, 10);
+            if (substring.join('') == '**********') {
+                res.push(' ');
+                continue
+            };
+            for (let j = 0; j < 10; j += 2) {
+                let symbol = substring.splice(0, 2).join('');
+                if (symbol != '00') {
+                    symbol == '10' ? letter.push('.') : letter.push('-')
+                }
+            }
+            res.push(MORSE_TABLE[letter.join('')])
+        }
+        return res.join('');
 }
 
 module.exports = {
